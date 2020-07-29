7ad93096ffc05c502a854ae2ea5a9d6ef1091b53
Vatsal Goel <goel.vatsal@outlook.com>
2020-07-28 23:20:02 -0400

    Deleting programs to clean repository

--- age.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) {
-    int age = 0;
-    printf("What is your age?");
-    scanf("%d", age);
-    printf("Your age is this: %d", age);
-    return 0;
-}
--- helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void) {
-    printf("Hello World!");
-    printf("Hello Earth!");
-    return 0;
-} 
