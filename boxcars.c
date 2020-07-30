76a8176696bb06c88fe8619456eacf77b6157713
goelvatsal <goel.vatsal@outlook.com>
2020-07-30 12:52:08 -0400

    Commit #2

--- boxcars.c
+++ boxcars.c
@@ -5,13 +5,13 @@ int main(void) {
     double array[50];
     int num_cars = 0;
 
-    printf("Hello\n");
+    // printf("Hello World!\n");
 
     scanf("%d", &num_cars);
 
     // loop over the the num cars and printing the array
-    for(int i = 0; i < num_cars; i++) {
-        printf("Weight of boxcar %d:", num_cars + 1);
-        scanf("%lf", &array[num_cars]);
+     for(int i = 0; i < num_cars; i++) {
+        printf("Weight of boxcar %d:", num_cars--);
+        scanf("%d", array[num_cars]);
     }
 }
