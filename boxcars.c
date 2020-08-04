09e3a4deb97287822d0763fb2ebd06d613771c98
Vatsal Goel <goel.vatsal@outlook.com>
2020-08-04 13:17:01 -0400

    Stage 2 of boxcars.c

--- boxcars.c
+++ boxcars.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 int main(void) {
     // declare variable
-    double total_weight = 0;
-    double array[50];
-    int num_cars = 0;
+    double array[51];
 
     // printf("Hello World!\n");
 
+    int num_cars = 0;
     printf("Number of boxcars: ");
     scanf("%d", &num_cars);
 
     // loop over the the num cars and printing the array
+     int i = 0;
      for(int i = 0; i < num_cars; i++) {
         printf("Weight of boxcar %d: ", i+1);
         scanf("%lf", &array[i]);
     }
 
     // loop over array and printing values
-
+    printf("What boxcar variable do you want to retrieve? ");
+    scanf("%lf", &array[51]);
+    printf("Boxcar %d weighs %lf", i-1, array[51]);
 }
