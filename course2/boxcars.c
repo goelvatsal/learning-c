c25f7cec4ab5474bfa7f98a8b5f986991da25f3b
Vatsal Goel <goel.vatsal@outlook.com>
2020-08-17 16:18:16 -0400

    The program doesn't work completely, but created a plan and need to fix displaying zeros.

--- course2/boxcars.c
+++ course2/boxcars.c
@@ -7,6 +7,7 @@ int main(void) {
 
     int num_cars = 0;
     // printf("Number of boxcars: ");
+    printf("\nInput: \n");
     scanf("%d", &num_cars);
 
     // loop over the the num cars and printing the array
@@ -32,18 +33,20 @@ int main(void) {
         sum = sum + array[i];
         // printf("Sum = %.2lf & i = %d\n", sum, i);
     }
-    printf("Sum = %.2lf \n", sum);
+    // printf("Sum = %.2lf \n", sum);
     double avg = (sum / num_cars);
-    printf("The average is %.2lf", avg);
+    // printf("The average is %.2lf\n", avg);
 
     // total_weight = array[i]
 
     // First, create if statement inside the for loop. Then, if the actual weight for each boxcar 
     // is more than the average, then calculate the diff between the boxcar and the avg (boxcar - avg) and display diff.
     // else then add the weight that is required for the boxcars to have the same weight.
+    printf("\nOutput: \n");
     for(int i = 0; i < num_cars; i++) {
+        // printf("Inside the loop %d times\n", i);
         if(avg < array[i]) {
-            printf("-%.0lf\n", avg - array[i]);
+            printf("%.1lf\n", avg - array[i]);
         } else {
             double diffadd = 0;
             diffadd = avg - array[1];
