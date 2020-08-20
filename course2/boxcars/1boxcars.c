47952c7a9f8a11a3fc2493aeae9199d913f3b32c
Vatsal Goel <goel.vatsal@outlook.com>
2020-08-20 14:41:38 -0400

    Fixed sum and avg issue

--- course2/boxcars/1boxcars.c
+++ course2/boxcars/1boxcars.c
@@ -3,37 +3,47 @@ int main(void) {
 
     // declare required variables (array)
     double array[52];
-    double num_cars = 0.0;
+    int num_cars = 0;
     printf("Input: \n");
 
     // read the num of items
-    scanf("%lf", &num_cars);
+    scanf("%d", &num_cars);
 
     // create a loop to read items into array
     for(int i = 0; i < num_cars; i++) {
-        scanf("%d", &array[i]);
+        scanf("%lf", &array[i]);
     }
     printf("\n");
 
     printf("Output: \n");
     for(int i = 0; i < num_cars; i++) {
-        printf("%d\n", array[i]);
+        printf("%.2lf\n", array[i]);
     }
         printf("\n");
-<<<<<<< HEAD
     
-    printf("Array elements = %d, %d, %d, %d, and %d\n", array[0], array[1], array[2], array[3], array[4]);
-=======
+    // printf("Array elements = %d, %d, %d, %d, and %d\n", array[0], array[1], array[2], array[3], array[4]);
 
->>>>>>> master
-    // find the average
-    double sum = array[0] + array[1] + array[2] + array[3] + array[4];
-    printf("Sum = %.2lf\n", sum); 
     
-    double avg = (sum / num_cars);
-    printf("avg = %.2lf\n", avg);
-    // debug: print the array using loop
-
     // iterate loop over the array and calculate diff and print the same
 
+    // find the average
+    double sum = 0.0;
+    for(int i = 0; i < num_cars; i++) {
+        // double temp = 0;
+        // temp += (array[i]);
+        sum = sum + array[i];
+        printf("Sum = %.2lf\n", sum);
+        printf("Array[%d] = %d", i, array[i]);
+    }
+
+    // int sum = array[0] + array[1] + array[2] + array[3] + array[4];
+    // printf("Sum = %d\n", sum); 
+    
+    int avg = (sum / num_cars);
+    // printf("avg = %d\n", avg);
+
+    // debug: print the array using loop
+    // for(int i = 0; i < num_cars; i++) {
+    //     printf("%d\n", array[i]);
+    // }
 }
