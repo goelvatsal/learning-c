7dbf7f1916d17765b86903ff197723e195fc2b1e
Vatsal Goel <goel.vatsal@outlook.com>
2020-08-24 15:20:54 -0400

    did final file

---
+++ course2/boxcars/finalbox.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(void) {
+    // declare nessasary variables
+    double array[51];
+    int num_cars = 0;
+    
+    // scanf for num_cars
+    printf("Input: \n");
+    scanf("%d", &num_cars);
+    for(int i = 0; i < num_cars; i++) {
+        scanf("%2lf", &array[i]);
+    }
+    printf("\n");
+    
+    // find sum and avg
+    double sum = 0.0;
+    double avg = 0.0;
+    for(int i = 0; i < num_cars; i++) {
+        sum = sum + array[i];
+        // printf("Sum = %.2lf\n", sum);
+        avg = (sum / num_cars);
+        // printf("Avg = %.2lf\n", avg);
+    }
+    
+    // create loop and find diff; display output
+    double diff = 0;
+    printf("Output: \n");
+    for(int i = 0; i < num_cars; i++) {
+        diff = avg - array[i];
+        printf("%.2lf\n", diff);
+    }
+}
