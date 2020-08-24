773bf5a831b3f6ab5d9152f4f21ac7faf89c699c
Vatsal Goel <goel.vatsal@outlook.com>
2020-08-24 10:49:02 -0400

    created outline

---
+++ course2/boxcars/2boxcars.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// create nessesary variables and arrays
+// scanf the numbers into array[i]
+// create loop to access and print numbers
+// create another loop to find sum 
+// create avg by dividing sum and num_cars 
+// create loop, find diff, and print output
