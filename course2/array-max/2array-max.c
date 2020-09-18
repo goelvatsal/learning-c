90da86b2f6c22c892d518d08be939b526304016e
Vatsal Goel <vatsalgoel@Visheshs-MBP.fios-router.home>
2020-09-17 21:15:55 -0400

    program completed

--- course2/array-max/2array-max.c
+++ course2/array-max/2array-max.c
@@ -5,13 +5,24 @@ int main(void) {
     double perpound[100];
 
     // create scanfs and arrays to find out price per pound and ingredients
-    scanf("%d", array_num);
-    printf("Ingredients: %d\n", array_num);
-    // for(int i = 0; i > array_num; i++) {
-    //     scanf("%lf", perpound[i]);
-    //     printf("Price Per Pound = %lf\n", perpound[i]);
-    // }
-}
+    scanf("%d", &array_num);
+    // printf("Ingredients: %d\n", array_num);
+    for(int i = 0; i < array_num; i++) {
+        scanf("%lf", &perpound[i]);
+        // printf("Price Per Pound = %lf; i = %d\n", perpound[i], i);
+    }
 
-// create another array for required amount per pound
-// print the price
+    // create another array for required amount per pound
+    double requiredp[100];
+    double sum;
+    double price;
+    for(int i = 0; i < array_num; i++) {
+        scanf("%lf", &requiredp[i]);
+        // printf("Required amount per pound: %lf\n", requiredp[i]);
+        price = perpound[i] * requiredp[i];
+        sum = sum + price;
+    }
+
+    // print the price
+    printf("%lf", sum);
+}
