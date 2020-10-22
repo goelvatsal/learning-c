4cab8a77c9aade1af1e106c20cbb0b9003979f40
Vatsal Goel <goel.vatsal@outlook.com>
2020-10-22 19:01:07 -0400

    workin on course

---
+++ course2/gridstar/gridstar.md
@@ -0,0 +1,19 @@
+Create a program that displays on the screen a square of n x n stars, with the integer n given as an input.
+
+Input:
+5
+
+Output:
+*****
+*****
+*****
+*****
+*****
+ 
+Input:
+3
+
+Output:
+***
+***
+***
---
+++ course2/gridstar/gridstar1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    // 1. declare nessasary variables
+    int starNum = 0;
+
+    // 2. create scanf to ask for input
+    scanf("%d", &starNum);
+
+    // 3. create for loop
+    for(int i = 0; i < starNum; i++) {
+        printf("")
+    }
+    // 4. printf the grid of stars
+
+
+}
