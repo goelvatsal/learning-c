2db50d37a7cf2e5e69e3e428d36a488530c9c606
Vatsal Goel <goel.vatsal@outlook.com>
2020-11-03 11:33:05 -0500

    working on arraystar

---
+++ course2/gridstars/gridstars1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    // 1. declare nessasary variables
+    int starNum = 0;
+
+    // 2. create scanf to ask for input
+    scanf("%d", &starNum);
+
+    // 3. create for loop & printf grid of stars
+    for(int i = 0; i < starNum; i++) {
+        printf("*");
+    }
+    printf("\n");
+}
