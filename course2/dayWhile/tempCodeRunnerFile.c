bcf1dc1e772edd4f52e37e779f327de92c01fbc6
Vatsal Goel <goel.vatsal@outlook.com>
2020-11-18 12:27:09 -0500

    completed dayWhile

--- course2/dayWhile/tempCodeRunnerFile.c
+++ course2/dayWhile/tempCodeRunnerFile.c
@@ -6,15 +6,15 @@ int main(void) {
     // scanf the amount of citizens total
     scanf("%d", &totalPop);
 
-    int daysPassed = 1, newInfect = 1, totalInfect = 1; 
+    int daysPassed = 0, newInfect = 1, totalInfect = 1; 
     // create while loop to increment day and infection rate
     while(totalInfect < totalPop) {
         daysPassed = daysPassed + 1;
-        newInfect = 2*totalInfect;
+        newInfect = 2*newInfect;
         totalInfect = totalInfect + newInfect;
         // printf("Day %d: New cases: %d: Total cases: %d\n", daysPassed, newInfect, totalInfect);
     }
     
     // printf the num of days to infect total pop
-    printf("%d\n", daysPassed);
+    printf("%d\n", daysPassed+1);
 }
