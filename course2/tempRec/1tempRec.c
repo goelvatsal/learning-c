ebf21e0e219f6b66fe90ca5bc161672ca58736ab
Vatsal Goel <goel.vatsal@outlook.com>
2020-11-18 15:18:21 -0500

    working on tempRec

---
+++ course2/tempRec/1tempRec.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    // declare nessasary variables
+    // scanf the min and max temps
+    // scanf the user temps
+    // when -999 scanned, end program
+    // printf
+}
