f02133bf6ad2dbe467d1bf0c6fb8d59488927a38
Vatsal Goel <goel.vatsal@outlook.com>
2020-12-27 11:57:54 -0500

    completed 5arraymax.c

--- course2/array-max/5arraymax.c
+++ course2/array-max/5arraymax.c
@@ -2,27 +2,30 @@
 int main(void) {
     // declare nessasary variables
     int numofitems = 0;
-    double perpound = 0, amountreq = 0, price[101];
 
     // scan the number of items
     scanf("%d", &numofitems);
     printf("numofitems = %d\n", numofitems);
 
     // scan the price per pound
+    double perpound[101];
     for(int i = 0; i < numofitems; i++) {
-        scanf("%lf", &perpound);
-        printf("price per pound required = %lf\n", perpound);
+        scanf("%lf", &perpound[i]);
+        printf("price per pound required = %lf\n", perpound[i]);
     }
 
     // scan the amount of each ingredient required
+    double amountreq[101];
     for(int i = 0; i < numofitems; i++) {
-        scanf("%lf", &amountreq);
-        printf("amountreq = %lf\n", amountreq);
+        scanf("%lf", &amountreq[i]);
+        printf("amountreq = %lf\n", amountreq[i]);
     }
 
     // multiply price per pound with amount of each ingredient
+    double oldprice, price;
     for(int i = 0; i < numofitems; i++) {
-        price[i] = perpound * amountreq;
-        printf("price = %lf\n", price[i]);
+        oldprice = perpound[i] * amountreq[i];
+        price = price + oldprice;
     }
+    printf("price = %lf\n", price);
 }
