98392770432a8cfa621abe519bc4bfc16bc153d1
Vatsal Goel <goel.vatsal@outlook.com>
2020-12-28 19:21:20 -0500

    working on 1longestword.c

---
+++ course2/longestWord/1longestWord.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    // declare all nessasary variables
+    int numofwords = 0;
+    int longestword = 0;
+    int i = 0;
+    // scan number of words
+    scanf("%d", &numofwords);
+    printf("numofwords = %d\n", numofwords);
+
+    // start a while loop
+    char words[101];
+    while(longestword != '\0') {
+        longestword = longestword + 1;
+    }
+}
