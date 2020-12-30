c28e07b25130bbe97873aeb7069bfeb857aa9f7c
Vatsal Goel <goel.vatsal@outlook.com>
2020-12-30 10:45:47 -0500

    working on searchingLetter

---
+++ course2/searchingLetter/1searchingLetter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    // declare nessasary variables
+    int tfound = 0;
+    char wordscanned = 0;
+    char word[51];
+    int foundornot = 0;
+
+    // create if statement inside while loop
+    int i = 0;
+    while(!foundornot && i < 51) {
+        if(item == t)
+    }
+    // search for letter T or t
+    // print 1 for t in first half; 2 for t in second half; -1 for no t at all
+}
