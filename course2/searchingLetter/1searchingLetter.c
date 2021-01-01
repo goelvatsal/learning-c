452f998751963475e47a2d5482dbf10e881ba5e0
Vatsal Goel <goel.vatsal@outlook.com>
2021-01-01 16:29:14 -0500

    moving to branch

--- course2/searchingLetter/1searchingLetter.c
+++ course2/searchingLetter/1searchingLetter.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main(void) {
     // declare nessasary variables
-    int tfound = 0;
-    char wordscanned = 0;
-    char word[51];
-    int foundornot = 0;
+    int tfound = 0, foundornot = 0, wordlen = 0;
+    char wordscanned = 0, word[51];
 
-    // create if statement inside while loop
-    int i = 0;
-    while(!foundornot && i < 51) {
-        if(item == t)
-    }
+    // scan word and find len
+    scanf("%s", word);
+    printf("word = %s\n", word);
+
+    
+
     // search for letter T or t
     // print 1 for t in first half; 2 for t in second half; -1 for no t at all
 }
