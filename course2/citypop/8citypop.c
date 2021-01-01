33d44f6164fb47fd1cfc78b4d3c3f1e221362adb
Vatsal Goel <goel.vatsal@outlook.com>
2021-01-01 13:53:50 -0500

    moving to branch

---
+++ course2/citypop/8citypop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    // declare all nessasary variables
+    int 
+    // scan the number of cities
+    // scan the pop of each city
+    // create if statement inside for loop
+    // printf cities that have pop of 10,000 or more
+}
