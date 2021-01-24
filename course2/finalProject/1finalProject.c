0078d6ec5b62db2abfbca1c5499c4af3b492a98a
Vatsal Goel <goel.vatsal@outlook.com>
2021-01-24 14:26:11 -0500

    fixed problem in 1finalProject.c

--- course2/finalProject/1finalProject.c
+++ course2/finalProject/1finalProject.c
@@ -9,7 +9,7 @@ int main(void) {
     int studentScores[51];
     for(int i = 0; i < numStudents; i++) {
         scanf("%d", &studentScores[i]);
-        printf("student score[%d] = %d\n", i, studentScores[i]);
+        printf("studentScore[%d] = %d\n", i, studentScores[i]);
     }
 
     // create second for loop to count students
@@ -19,28 +19,29 @@ int main(void) {
         printf("studentName[%d] = %s\n", i, studentName);
     }
 
-    int studentRank[51], swap = 0;
-    char charswap = '\0';
+    // int studentRank[51];
+    int swap = 0;
+    // char charswap = '\0';
     for(int i = 0; i < numStudents; i++) {
-        if(studentScores[i + 1] == '0') {
-            break;
-        }
-
         if(studentScores[i] > studentScores[i + 1]) {
+            if(studentScores[i + 1] == 0) {
+                break;
+            }
             swap = studentScores[i + 1];
             studentScores[i + 1] = studentScores[i];
             studentScores[i] = swap;
         }
-
-        if(studentScores[i + 1] == swap) {
-            charswap = studentName[i + 1];
-            studentName[i + 1] = studentName[i];
-            studentName[i] = charswap;
-        }
+    }
+        // if(studentScores[i] == swap) {
+        //     charswap = studentName[i + 1];
+        //     studentName[i + 1] = studentName[i];
+        //     studentName[i] = charswap;
+        // }
 
             // printf("studentScores == %d\n", studentScores[i]);
 
              // print the scores
-            printf("%s score is %d\n", studentName, studentScores[i]);
-    }
+             for(int i = 0; i < numStudents; i++) {
+                printf("%s score is %d\n", studentName, studentScores[i]);
+             }
 }
