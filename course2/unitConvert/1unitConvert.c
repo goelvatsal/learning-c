2e9ba5b893638f88fdf2962c2993dce5f496be22
Vatsal Goel <goel.vatsal@outlook.com>
2021-02-17 12:33:56 -0500

    working on unitConvert

--- course2/unitConvert/1unitConvert.c
+++ course2/unitConvert/1unitConvert.c
@@ -6,21 +6,22 @@
         printf("num of units to convert = %d\n", numofconversions);
 
         // create 4 if statements in a loop to find out what unit to convert tosl
+        double numtoconvert[51];
+        char scanned_unit[5];
         for(int i = 0; i < numofconversions; i++) {
-            double numtoconvert[51];
-            char scanned_unit[51];
-            scanf("%lf %s", &numtoconvert, scanned_unit);
-            printf("numtoconvert = %.2lf, scanned_unit = %s\n", numtoconvert, scanned_unit);
-
-            // // char changed_unit[5];
-            // if(scanned_unit == "g") {
-            //     printf("changed_unit = lbs\n");
-            // } else if(scanned_unit == "m") {
-            //     printf("changed_unit = ft\n");
-            // } else if(scanned_unit == "c") {
-            //     printf("changed_unit = f");
-            // }
+            scanf("%lf %s", &numtoconvert[i], scanned_unit);
         }
+            // char changed_unit[5];
+            for(int i = 0; i < numofconversions; i++) {
+                if(scanned_unit[i] == 'g') {
+                    printf("changed_unit = lbs\n");
+                } else if(scanned_unit[i] == 'm') {
+                    printf("changed_unit = ft\n");
+                } else if(scanned_unit[i] == 'c') {
+                    printf("changed_unit = f");
+                }
+                printf("numtoconvert = %.2lf, scanned_unit = %s\n", numtoconvert[i], scanned_unit);
+            }
         // convert each unit inside if statement
         // when printing, print converted_unit and then the changed unit
     }
