a1504f00d025afd080c05761ec4e415071456a9d
Vatsal Goel <goel.vatsal@outlook.com>
2021-03-22 19:40:21 -0400

    working on memorySpace

--- course2/memorySpace/1memorySpace.c
+++ course2/memorySpace/1memorySpace.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
 int main(void) {
-    
+    // scan the num of tracking codes
+    int numofCodes
+    // set up if statement for each letter
+    // use sizeof() function
+    // printf the num of bytes used by the tracking code
 }
