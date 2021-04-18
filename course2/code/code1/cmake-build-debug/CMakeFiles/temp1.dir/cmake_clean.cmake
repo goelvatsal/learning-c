2cb9d03549589f01ed44db4eb07480514ce75eba
Vatsal Goel <goel.vatsal@outlook.com>
2021-04-18 17:14:24 -0400

    test files pushing from clion

---
+++ course2/code/code1/cmake-build-debug/CMakeFiles/temp1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temp1.dir/main.cpp.o"
+  "temp1"
+  "temp1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ course2/practice-programs/print-hello-world.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int i = 0;
+    printf("What is your age?\n");
+    scanf("%d", &i);
+    printf("Your age is: %d\n", i);
+}
