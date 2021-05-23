d4c2f378c442f6aa53c77f63cdbd7f0054db5a93
Vatsal Goel <goel01@elitebook840.fios-router.home>
2021-05-23 16:38:22 -0400

    creating test programs

--- course2/testPrograms/print-first-Name.cpp
+++ course2/testPrograms/print-first-Name.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 int main(void) {
     char firstName[52];
     printf("What is your first name?\n");
-    scanf("%s", firstName);
+    scanf("%s", &firstName);
 //    printf("%s", firstName);
     for (int i = 0; i < 10; i++) {
         printf("char[%d]=%c\n", i, firstName[i]);
---
+++ course2/testPrograms/print-user-age.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+    int userAge = 0;
+    printf("What is your age? ");
+    scanf("%d", &userAge);
+    printf("userAge == %d!\n", userAge);
+}
